06cb0a77ec3b8965443fa6e94ff3713db4868509
Pratik Ghotane <pratikghotane@Pratiks-MacBook-Air.local>
2024-11-07 23:12:13 +0530

    priceing card button added

--- src/components/PriceingCard.jsx
+++ src/components/PriceingCard.jsx
@@ -5,6 +5,9 @@ const PriceingCard = ({ totalquantity, price }) => {
         <div className='border p-3 rounded-lg mt-3 mx-4 py-2'>
             <h2 className='text-3xl font-semibold'>Subtotal ({totalquantity}):</h2>
             <span className='text-4xl font-extrabold'>₹{price.toFixed(2)}</span>
+            <div className={`cursor-pointer border py-3 px-5 rounded-md text-center mt-4 bg-yellow-300`}>
+                <button> Buy Now</button>
+            </div>
         </div>
     )
 }
